cbbb551718ffa51bc3b83d715b006cdd045b002a
Rune <runew0lf@gmail.com>
2023-04-02 09:30:38 +0100

    updated docstrings

--- downloader.py
+++ downloader.py
@@ -7,13 +7,43 @@ logger = logging.getLogger(__name__)
 
 
 class TqdmForPyTube(tqdm):
+    """A custom tqdm progress bar for use with PyTube.
+
+    This class extends the tqdm progress bar to provide a custom progress bar for use with PyTube.
+
+    Attributes:
+        total: The total size of the file being downloaded.
+    """
+
     def on_progress(self, stream: Stream, chunk: bytes, bytes_remaining: int) -> None:
+        """Updates the progress bar with the current download progress.
+
+        Args:
+            stream: The stream being downloaded.
+            chunk: The most recent chunk of data received.
+            bytes_remaining: The number of bytes remaining to be downloaded.
+
+        Returns:
+            None
+        """
         self.total = stream.filesize
         bytes_downloaded = self.total - bytes_remaining
         self.update(bytes_downloaded - self.n)
 
 
 def download_video(url: str, filename: str, on_progress_callback: Callable = None) -> None:
+    """Downloads a video from YouTube.
+
+    This function downloads a video from YouTube and saves it to the specified file.
+
+    Args:
+        url: The URL of the video to download.
+        filename: The name of the file to save the video to.
+        on_progress_callback: An optional callback function to be called as the download progresses.
+
+    Returns:
+        None
+    """
     logger.info("Downloading video...")
     with TqdmForPyTube(unit="bytes") as pbar:
         yt = YouTube(url)
--- transcriber.py
+++ transcriber.py
@@ -10,17 +10,52 @@ logger = logging.getLogger(__name__)
 
 
 class CustomProgressBar(tqdm):
+    """A custom progress bar for use with WhisperAI.
+
+    This class extends the tqdm progress bar to provide a custom progress bar for use with WhisperAI.
+
+    Attributes:
+        current: The current progress of the task being tracked.
+    """
+
     def __init__(self, *args, **kwargs):
+        """Initializes a new instance of the CustomProgressBar class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            None
+        """
         super().__init__(*args, **kwargs)
         self.current = self.n
 
     def update(self, n: int):
+        """Updates the progress bar with the current progress.
+
+        Args:
+            n: The number of iterations to increment the progress bar by.
+
+        Returns:
+            None
+        """
         super().update(n)
         self.current += n
         print(f"\rProgress: {self.current}/{self.total} ({self.current / self.total * 100:.2f}%)", end="", flush=True)
 
 
 def transcribe_video(filename: str) -> Dict:
+    """Transcribes a video using WhisperAI.
+
+    This function transcribes a video using WhisperAI and returns the transcription as a dictionary.
+
+    Args:
+        filename: The name of the file containing the video to transcribe.
+
+    Returns:
+        A dictionary containing the transcription of the video.
+    """
     logger.info("Transcribing video...")
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = whisper.load_model("small.en", device=device)
@@ -33,6 +68,17 @@ def transcribe_video(filename: str) -> Dict:
 
 
 def save_transcription_to_vtt(transcription: Dict, save_target: str) -> None:
+    """Saves a transcription to a WebVTT file.
+
+    This function saves a transcription to a WebVTT file.
+
+    Args:
+        transcription: The transcription to save.
+        save_target: The name of the file to save the transcription to.
+
+    Returns:
+        None
+    """
     logger.info("Saving transcription...")
     with open(save_target, "w", encoding="utf-8") as file:
         file.write("WEBVTT\n\n")
